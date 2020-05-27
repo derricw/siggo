126a7527c9d3d083c6eeaf718b9ba96d52fce069
Derric Williams <dwilliams@domaintools.com>
2020-05-27 14:36:03 -0700

    moved ui module to widgets

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,16 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
+
+	"github.com/rivo/tview"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/derricw/siggo/model"
+	"github.com/derricw/siggo/signal"
+	"github.com/derricw/siggo/widgets"
 )
 
 var Mock string
@@ -19,7 +27,45 @@ var rootCmd = &cobra.Command{
 	Short: "siggo is a terminal gui for signal-cli",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		outputFile, err := os.Create("/tmp/siggo.log")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if Debug {
+			log.SetLevel(log.DebugLevel)
+		}
+		log.SetOutput(outputFile)
+
+		cfg, err := model.GetConfig()
+		if err != nil {
+			log.Fatal("failed to read config @ %s", model.DefaultConfigPath())
+		}
+		if cfg.UserNumber == "" {
+			log.Fatalf("no user phone number configured @ %s", model.DefaultConfigPath())
+		}
+
+		var signalAPI model.SignalAPI = signal.NewSignal(cfg.UserNumber)
+		if Mock != "" {
+			b, err := ioutil.ReadFile(Mock)
+			if err != nil {
+				log.Fatalf("couldn't open mock data")
+			}
+			signalAPI = signal.NewMockSignal(cfg.UserNumber, b)
+		}
+
+		s := model.NewSiggo(signalAPI, cfg)
+
+		s.ReceiveForever()
+		//tview.Styles.PrimitiveBackgroundColor = tcell.ColorDefault
+		app := tview.NewApplication()
+		chatWindow := widgets.NewChatWindow(s, app)
+
+		// finally, start the tview app
+		if err := app.SetRoot(chatWindow, true).SetFocus(chatWindow).Run(); err != nil {
+			panic(err)
+		}
+		// clean up when we're done
+		chatWindow.Quit()
 	},
 }
 
