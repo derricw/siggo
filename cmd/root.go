b7a75da2791738edd1ec598cad153ca67e12a7c5
Joshua Crowgey <jcrowgey@uw.edu>
2020-06-01 22:50:55 -0700

    Add log_file to config options
    Writes the log messages to a user-configurable location.  Still defaults
    to "/tmp/siggo.log" when the config contains no information.

--- cmd/root.go
+++ cmd/root.go
@@ -14,12 +14,30 @@ import (
 	"github.com/derricw/siggo/widgets"
 )
 
-var Mock string
-var Debug bool
+var (
+	Mock  string
+	Debug bool
+)
+
+const defaultLogPath = "/tmp/siggo.log"
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&Mock, "mock", "m", "", "mock mode (uses example data)")
 	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "debug logging")
+}
+
+func initLogging(cfg *model.Config) {
+	if cfg.LogFilePath == "" {
+		cfg.LogFilePath = defaultLogPath
+	}
+	logFile, err := os.Create(cfg.LogFilePath)
+	if err != nil {
+		log.Fatal("error creating log file: %v %v", cfg.LogFilePath, err)
+	}
+	if Debug {
+		log.SetLevel(log.DebugLevel)
+	}
+	log.SetOutput(logFile)
 }
 
 var rootCmd = &cobra.Command{
@@ -27,19 +45,13 @@ var rootCmd = &cobra.Command{
 	Short: "siggo is a terminal gui for signal-cli",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		outputFile, err := os.Create("/tmp/siggo.log")
-		if err != nil {
-			log.Fatal(err)
-		}
-		if Debug {
-			log.SetLevel(log.DebugLevel)
-		}
-		log.SetOutput(outputFile)
-
 		cfg, err := model.GetConfig()
 		if err != nil {
 			log.Fatal("failed to read config @ %s", model.DefaultConfigPath())
 		}
+
+		initLogging(cfg)
+
 		if cfg.UserNumber == "" {
 			log.Fatalf("no user phone number configured @ %s", model.DefaultConfigPath())
 		}
