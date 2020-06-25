4f3a55310ad0739a6d65b4f52cd67b990cb8cc4b
Derric Williams <derricw@gmail.com>
2020-06-24 22:58:49 -0700

    v0.4.0

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	GitCommit string
-	Version   = "0.2.1"
+	Version   = "0.4.0"
 	BuildDate = ""
 	GoVersion = runtime.Version()
 	OsArch    = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
