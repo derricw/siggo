f64e6ccce3471909270e8bd6c776b7641d01150e
Derric Williams <derricw@gmail.com>
2021-07-30 17:27:57 -0700

    bump version to 0.11.0

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	GitCommit string
-	Version   = "0.10.0"
+	Version   = "0.11.0"
 	BuildDate = ""
 	GoVersion = runtime.Version()
 	OsArch    = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
